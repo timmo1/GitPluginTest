2a2d52dc85dd9ed510b635bd8a8ab94399cc3f73
Tim Scriven <timscriven180@gmail.com>
2016-03-20 11:21:09 +0000

    modified print statement.

--- src/packt/jee/eclipse/git/GITTestApplication.java
+++ src/packt/jee/eclipse/git/GITTestApplication.java
@@ -3,7 +3,7 @@ package packt.jee.eclipse.git;
 public class GITTestApplication {
 
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 	
 }
